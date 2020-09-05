f57ec58ddf2c11720ec68185068ed99165cb179e
Matheus Monteiro <matheusferretti96@gmail.com>
2020-09-05 00:51:20 +0000

    reactCounterJSX

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,43 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+function Counter(props) {
+	let mystyle = {
+		color: "white",
+		backgroundColor: "#07090F",
+		padding: "10px",
+		fontFamily: "Arial",
+		fontSize: "40px",
+		borderRadius: "20px"
+	};
+	return (
+		<div className="container">
+			<div className="row text-center" style={{ background: "black" }}>
+				<div className="col-sm pt-5 mr-3">
+					<i className="far fa-clock fa-3x" />
+				</div>
+				<div className="col-sm pt-5 mr-3" style={mystyle}>
+					6
+				</div>
+				<div className="col-sm pt-5 mr-3" style={mystyle}>
+					5
+				</div>
+				<div className="col-sm pt-5 mr-3" style={mystyle}>
+					4
+				</div>
+				<div className="col-sm pt-5 mr-3" style={mystyle}>
+					3
+				</div>
+				<div className="col-sm pt-5 mr-3" style={mystyle}>
+					2
+				</div>
+				<div className="col-sm pt-5 mr-3" style={mystyle}>
+					1
+				</div>
+			</div>
+		</div>
+	);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Counter />, document.querySelector("#app"));
