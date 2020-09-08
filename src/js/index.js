34601d72e4fbe0389447a2dac4a1418a3aaaeef0
Matheus Monteiro <matheusferretti96@gmail.com>
2020-09-08 21:31:36 +0000

    FinishedCounterJSX

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -9,43 +10,76 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
+
 function Counter(props) {
 	let mystyle = {
+		display: "inline-block",
+		fontSize: "60px",
+		lineHeight: "60px",
+		background: "#07090F",
 		color: "white",
-		backgroundColor: "#07090F",
 		padding: "10px",
-		fontFamily: "Arial",
-		fontSize: "40px",
-		borderRadius: "20px"
+		margin: "3px"
 	};
 	return (
 		<div className="container">
-			<div className="row text-center" style={{ background: "black" }}>
-				<div className="col-sm pt-5 mr-3">
+			<div
+				className="row"
+				style={{ background: "black", textAlign: "center" }}>
+				<div className="col-sm pt-5 mr-3" style={{ color: "white" }}>
 					<i className="far fa-clock fa-3x" />
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					6
+					{props.digitSix}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					5
+					{props.digitFive}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					4
+					{props.digitFour}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					3
+					{props.digitThree}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					2
+					{props.digitTwo}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					1
+					{props.digitOne}
 				</div>
 			</div>
 		</div>
 	);
 }
 
-//render your react application
-ReactDOM.render(<Counter />, document.querySelector("#app"));
+Counter.propTypes = {
+	digitSix: "PropTypes.number",
+	digitFive: "PropTypes.number",
+	digitFour: "PropTypes.number",
+	digitThree: "PropTypes.number",
+	digitTwo: "PropTypes.number",
+	digitOne: "PropTypes.number"
+};
+
+let simpleCounter = 0;
+setInterval(function() {
+	//render your react application
+	const six = Math.floor(simpleCounter / 100000);
+	const five = Math.floor(simpleCounter / 10000);
+	const four = Math.floor(simpleCounter / 1000);
+	const three = Math.floor(simpleCounter / 100);
+	const two = Math.floor(simpleCounter / 10);
+	const one = Math.floor(simpleCounter / 1);
+	simpleCounter++;
+	ReactDOM.render(
+		<Counter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+			digitFive={five}
+			digitSix={six}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
